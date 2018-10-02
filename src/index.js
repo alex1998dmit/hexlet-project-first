fde719e62f3205997ffe73efe586849ae4af5612
alex1998dmit <alex1998dmit@gmail.com>
2018-10-02 21:28:10 +0300

    change to linter

--- src/index.js
+++ src/index.js
@@ -12,33 +12,27 @@ const generateNum = () => {
   return Math.round(min + Math.random() * (max - min));
 };
 
-const getUserAnswer = () => {
+const questionGenerate = (num) => {
+  console.log(`Question: ${num}`);
   const userAnswer = readlineSync.question('Your answer: ');
   return userAnswer;
 };
 
-const questionGenerate = (num) => {
-    console.log(`Question: ${num}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    return userAnswer;
-}
-
-const checParity = (userName, numWinGames = 1) => {
-    const numGenerate = generateNum();
-    const userAnswer = questionGenerate(numGenerate);
-    const rightAnswer = (numGenerate % 2 === 0) ? 'yes' : 'no';
-
-    if(numWinGames === 3){
-        return `Congratulations, ${userName}!`;
-    }
-    if(rightAnswer === userAnswer){
-        console.log(`Correct!`);
-        numWinGames = numWinGames + 1;
-        return checParity(userName, numWinGames);
-    } else {
-        const looseStr = `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. Let's try again, ${userName}!`;
-        return looseStr;
-    }
+const checParity = (userName, numWin = 1) => {
+  let numWinGames = numWin;
+  const numGenerate = generateNum();
+  const userAnswer = questionGenerate(numGenerate);
+  const rightAnswer = (numGenerate % 2 === 0) ? 'yes' : 'no';
+  if (numWinGames === 3) {
+    return `Congratulations, ${userName}!`;
+  }
+  if (rightAnswer === userAnswer) {
+    console.log('Correct!');
+    numWinGames += 1;
+    return checParity(userName, numWinGames);
+  }
+  const looseStr = `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. Let's try again, ${userName}!`;
+  return looseStr;
 };
 
-export { sayHiToUser, checParity};
+export { sayHiToUser, checParity };
