7cc5c5ab1f974d873ed6f7927845210b43105333
alex1998dmit <alex1998dmit@gmail.com>
2018-10-06 16:55:12 +0300

    Change prog func str

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -3,24 +3,24 @@ import startGame from '..';
 import generateRandomNum from '../utils';
 
 const gameDesc = 'What number is missing in this progression?';
+const countProgressElemnts = 10;
+
+
+const generateProgStr = (numIter, progrNumber, progStr, progEmpty, progDiff) => {
+  if (numIter > countProgressElemnts) {
+    return progStr;
+  }
+  const newIter = numIter + 1;
+  const newProgStr = numIter === progEmpty ? `${progStr}...` : `${progStr} ${progrNumber} `;
+  const nextNumber = progrNumber + progDiff;
+  return generateProgStr(newIter, nextNumber, newProgStr, progEmpty, progDiff);
+};
 
 const findProgressPair = () => {
   const progEmpty = generateRandomNum(1, 9);
   const progStart = generateRandomNum(1, 10);
   const progDiff = generateRandomNum(1, 5);
-  const countProgressElemnts = 10;
-
-  const generateProgStr = (numIter, progrNumber, progStr) => {
-    if (numIter > countProgressElemnts) {
-      return progStr;
-    }
-    const newIter = numIter + 1;
-    const newProgStr = numIter === progEmpty ? `${progStr}...` : `${progStr} ${progrNumber} `;
-    const nextNumber = progrNumber + progDiff;
-    return generateProgStr(newIter, nextNumber, newProgStr);
-  };
-
-  const question = `${generateProgStr(1, progStart, '')}`;
+  const question = `${generateProgStr(1, progStart, '', progEmpty, progDiff)}`;
   const rightAnswer = progStart + (progEmpty - 1) * progDiff;
   return cons(question, String(rightAnswer));
 };
