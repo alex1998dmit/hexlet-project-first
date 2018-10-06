20875c258a10f97f6c4d289d1f99ea479c47336b
alex1998dmit <alex1998dmit@gmail.com>
2018-10-06 17:26:48 +0300

    Change prg

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,23 +3,22 @@ import startGame from '..';
 import generateRandomNum from '../utils';
 
 const gameDesc = 'What number is missing in this progression?';
-const countProgressElemnts = 10;
-
-const generateProgStr = (numIter, progrNumber, progStr, emptyIndex, diff) => {
-  if (numIter > countProgressElemnts) {
-    return progStr;
-  }
-  const newIter = numIter + 1;
-  const newProgStr = numIter === emptyIndex ? `${progStr}...` : `${progStr} ${progrNumber} `;
-  const nextNumber = progrNumber + diff;
-  return generateProgStr(newIter, nextNumber, newProgStr, emptyIndex, diff);
-};
+const length = 10;
 
 const findProgressPair = () => {
   const emptyIndex = generateRandomNum(1, 9);
   const firstNum = generateRandomNum(1, 10);
   const diff = generateRandomNum(1, 5);
-  const question = `${generateProgStr(1, firstNum, '', emptyIndex, diff)}`;
+  const generatestr = (iter, num, str) => {
+    if (iter > length) {
+      return str;
+    }
+    const newIter = iter + 1;
+    const newstr = iter === emptyIndex ? `${str}...` : `${str} ${num} `;
+    const nextNumber = num + diff;
+    return generatestr(newIter, nextNumber, newstr, emptyIndex, diff);
+  };
+  const question = `${generatestr(1, firstNum)}`;
   const rightAnswer = firstNum + (emptyIndex - 1) * diff;
   return cons(question, String(rightAnswer));
 };
